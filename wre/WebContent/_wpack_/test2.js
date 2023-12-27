f160946d3924036ded8d2525dddb3e3143665142
Dormailler <gerrerd@naver.com>
2023-12-28 07:11:21 +0900

    웹스퀘어 연습

--- wre/WebContent/_wpack_/test2.js
+++ wre/WebContent/_wpack_/test2.js
@@ -1,7 +1,7 @@
-/*amd /prac2.xml 1219 4f77b804650b5db63eb7ed947d5155575fe513810aed829d17d044de38f6eb51 */
+/*amd /test2.xml 722 a7e933c9ba6178a6555c878a14870e0cdd0ffc916a32af4e5a4576abb089ee3a */
 define({declaration:{A:{version:'1.0',encoding:'UTF-8'}},E:[{T:1,N:'html',A:{xmlns:'http://www.w3.org/1999/xhtml','xmlns:ev':'http://www.w3.org/2001/xml-events','xmlns:w2':'http://www.inswave.com/websquare','xmlns:xf':'http://www.w3.org/2002/xforms'},E:[{T:1,N:'head',E:[{T:1,N:'w2:type',E:[{T:3,text:'COMPONENT'}]},{T:1,N:'w2:buildDate'},{T:1,N:'w2:MSA'},{T:1,N:'xf:model',E:[{T:1,N:'w2:dataCollection',A:{baseNode:'map'}},{T:1,N:'w2:workflowCollection'}]},{T:1,N:'w2:layoutInfo'},{T:1,N:'w2:publicInfo',A:{method:''}},{T:1,N:'script',A:{lazy:'false',type:'text/javascript'},E:[{T:4,cdata:function(scopeObj){with(scopeObj){
 scwin.onpageload = function() {
 	
 };
 
-}}}]}]},{T:1,N:'body',A:{'ev:onpageload':'scwin.onpageload'},E:[{T:1,N:'w2:autoComplete',A:{editType:'select',id:'',style:'width: 148px;height: 21px;',submenuSize:'auto',search:'start',allOption:'',chooseOption:'',ref:''},E:[{T:1,N:'w2:choices',E:[{T:1,N:'w2:item',E:[{T:1,N:'w2:label',E:[{T:4,cdata:'1'}]},{T:1,N:'w2:value'}]},{T:1,N:'w2:item',E:[{T:1,N:'w2:label',E:[{T:4,cdata:'2'}]},{T:1,N:'w2:value'}]},{T:1,N:'w2:item',E:[{T:1,N:'w2:label',E:[{T:4,cdata:'3'}]},{T:1,N:'w2:value'}]}]}]},{T:1,N:'xf:textarea',A:{id:'',style:'width:300px;height:38px;'}}]}]}]})
+}}}]}]},{T:1,N:'body',A:{'ev:onpageload':'scwin.onpageload'}}]}]})
