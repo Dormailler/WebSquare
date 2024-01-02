7deb16163a1471c076e2072870bb525b07ebaeaa
Dormailler <gerrerd@naver.com>
2024-01-02 22:07:04 +0900

    웹스퀘어 연습

---
+++ wre/WebContent/_wpack_/ex/drawMode.js
@@ -0,0 +1,7 @@
+/*amd /ex/drawMode.xml 970 e02d096c060651823c2c60006e969aa0ce0cb928699a0605eeb9cb525e03d2c8 */
+define({declaration:{A:{version:'1.0',encoding:'UTF-8'}},E:[{T:1,N:'html',A:{xmlns:'http://www.w3.org/1999/xhtml','xmlns:ev':'http://www.w3.org/2001/xml-events','xmlns:w2':'http://www.inswave.com/websquare','xmlns:xf':'http://www.w3.org/2002/xforms'},E:[{T:1,N:'head',E:[{T:1,N:'w2:type',E:[{T:3,text:'COMPONENT'}]},{T:1,N:'w2:buildDate'},{T:1,N:'w2:MSA'},{T:1,N:'xf:model',E:[{T:1,N:'w2:dataCollection',A:{baseNode:'map'}},{T:1,N:'w2:workflowCollection'}]},{T:1,N:'w2:layoutInfo'},{T:1,N:'w2:publicInfo',A:{method:''}},{T:1,N:'script',A:{lazy:'false',type:'text/javascript'},E:[{T:4,cdata:function(scopeObj){with(scopeObj){
+scwin.onpageload = function() {
+	
+};
+
+}}}]}]},{T:1,N:'body',A:{'ev:onpageload':'scwin.onpageload'},E:[{T:1,N:'xf:input',A:{id:'',style:'position:absolute;top:95px;left:191px;width:233px;height:136px;'}},{T:1,N:'w2:inputCalendar',A:{id:'',style:'position:absolute;top:129px;left:174px;width:375px;height:85px;',calendarValueType:'yearMonthDate'}}]}]}]})
---
+++ wre/WebContent/_wpack_/ex/overflow.js
@@ -0,0 +1,7 @@
+/*amd /ex/overflow.xml 1109 fa115e67614b3254d81d63c0eeca98f8bf855943147b664b85283a7b7c906c4a */
+define({declaration:{A:{version:'1.0',encoding:'UTF-8'}},E:[{T:1,N:'html',A:{xmlns:'http://www.w3.org/1999/xhtml','xmlns:ev':'http://www.w3.org/2001/xml-events','xmlns:w2':'http://www.inswave.com/websquare','xmlns:xf':'http://www.w3.org/2002/xforms'},E:[{T:1,N:'head',E:[{T:1,N:'w2:type',E:[{T:3,text:'COMPONENT'}]},{T:1,N:'w2:buildDate'},{T:1,N:'w2:MSA'},{T:1,N:'xf:model',E:[{T:1,N:'w2:dataCollection',A:{baseNode:'map'}},{T:1,N:'w2:workflowCollection'}]},{T:1,N:'w2:layoutInfo'},{T:1,N:'w2:publicInfo',A:{method:''}},{T:1,N:'script',A:{lazy:'false',type:'text/javascript'},E:[{T:4,cdata:function(scopeObj){with(scopeObj){
+scwin.onpageload = function() {
+	
+};
+
+}}}]}]},{T:1,N:'body',A:{'ev:onpageload':'scwin.onpageload'},E:[{T:1,N:'xf:group',A:{id:'',style:'border:2px solid black;overflow: scroll;;'},E:[{T:1,N:'xf:input',A:{id:'',style:'width:200px;height:40px;margin:5px;background-color: orange;'}},{T:1,N:'xf:input',A:{id:'',style:'width:246px;height:40px;margin:5px;background-color:red;'}},{T:1,N:'xf:input',A:{id:'',style:'width:163px;height:40px;margin:5px;background-color:green;float:right;'}}]}]}]}]})
