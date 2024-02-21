0451cc68170bebd0410518b4043eed5ea7ca7044
Dormailler <gerrerd@naver.com>
2024-02-22 01:25:51 +0900

    웹스퀘어 연습

---
+++ wre/WebContent/_wpack_/ex/Iframe.js
@@ -0,0 +1,11 @@
+/*amd /ex/Iframe.xml 1176 cab7a44fd9e7674bb85e2f9b7aa9d3a5afdfa9ea4a247615b00ee457384a7572 */
+define({declaration:{A:{version:'1.0',encoding:'UTF-8'}},E:[{T:1,N:'html',A:{xmlns:'http://www.w3.org/1999/xhtml','xmlns:ev':'http://www.w3.org/2001/xml-events','xmlns:w2':'http://www.inswave.com/websquare','xmlns:xf':'http://www.w3.org/2002/xforms'},E:[{T:1,N:'head',E:[{T:1,N:'w2:type',E:[{T:3,text:'COMPONENT'}]},{T:1,N:'w2:buildDate'},{T:1,N:'w2:MSA'},{T:1,N:'xf:model',E:[{T:1,N:'w2:dataCollection',A:{baseNode:'map'}},{T:1,N:'w2:workflowCollection'}]},{T:1,N:'w2:layoutInfo'},{T:1,N:'w2:publicInfo',A:{method:''}},{T:1,N:'script',A:{lazy:'false',type:'text/javascript'},E:[{T:4,cdata:function(scopeObj){with(scopeObj){
+scwin.onpageload = function() {
+	
+};
+
+scwin.trigger1_onclick = function(e) {
+    if1.setSrc("Exam1.xml");
+};
+
+}}}]}]},{T:1,N:'body',A:{'ev:onpageload':'scwin.onpageload'},E:[{T:1,N:'w2:iframe',A:{src:'Group.xml',style:'position:absolute; top:38px; left:56px; width:301px; height:258px; ',id:'if1',spa:'true',spaReplaceHistory:'true'}},{T:1,N:'xf:trigger',A:{type:'button',id:'trigger1',style:'position:absolute; top:319px; left:55px; width:131px; height:43px; ','ev:onclick':'scwin.trigger1_onclick'},E:[{T:1,N:'xf:label',E:[{T:4,cdata:'setSrc()'}]}]}]}]}]})
